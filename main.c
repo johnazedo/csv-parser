d661c56d44104a4a985a00f893df3ee37f812442
João Pedro Limão <jplimao077@gmail.com>
2025-10-22 13:29:38 -0300

    create static readline

--- main.c
+++ main.c
@@ -1,12 +1,37 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define ERROR_TO_READ_FILE -1
+#define FAILURE -1
+#define MIN_LINE_SIZE 128
+#define MAX_LINE_SIZE 128
+#define SUCCESS 0
+
 // ## is used to handle the case when no variable arguments are passed
 #define LOG(fmt, ...)                                                          \
-  printf("%s:%d" fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+  printf("[%s:%d] " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 typedef unsigned char byte;
+
+char *readline(char *src, size_t max_size, FILE *file) {
+  size_t len = 0;
+  int c;
+
+  while (1) {
+    c = fgetc(file);
+    if (c == EOF || c == '\n') {
+      break;
+    }
+
+    src[len++] = (byte)c;
+  }
+
+  if (len == 0 && c == EOF) {
+    return NULL;
+  }
+
+  return src;
+}
 
 int main(int argc, char *argv[]) {
   int i = 0;
@@ -31,6 +56,16 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
+  char line[MAX_LINE_SIZE];
+  char *ret;
+  while (1) {
+    ret = readline(line, MAX_LINE_SIZE, fptr);
+    if (ret == NULL) {
+      break;
+    }
+    LOG("line=%s", line);
+    memset(line, 0, sizeof(line));
+  }
   fclose(fptr);
   free(namefile);
   return EXIT_SUCCESS;
