465bd9aeadaf0c9fb4d0600f108a77ab50ad1ab6
João Pedro Limão <jplimao077@gmail.com>
2025-10-22 09:12:59 -0300

    try to create a readline function

--- main.c
+++ main.c
@@ -2,6 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define ERROR_TO_READ_FILE -1
+
+typedef unsigned char byte;
+
+int readline(FILE *fptr, byte *out) {
+  int n = 0;
+  int size = 128;
+  // If out has been allocated previously, this will cause a memory leak
+  out = malloc(size + 1);
+  int ch;
+
+  return ERROR_TO_READ_FILE;
+}
+
 int main(int argc, char *argv[]) {
   int i = 0;
 
@@ -18,6 +32,14 @@ int main(int argc, char *argv[]) {
 
   printf("\nfilename=%s\n", namefile);
 
+  FILE *fptr;
+  fptr = fopen(namefile, "r");
+  if (fptr == NULL) {
+    printf("Error! Could not open file %s\n", namefile);
+    return EXIT_FAILURE;
+  }
+
+  fclose(fptr);
   free(namefile);
   return EXIT_SUCCESS;
 }
